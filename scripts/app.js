b1696eb3e9bfc52e5a44ad2d69c62c4590fd4d40
Maira Umer <mairah.khan08@gmail.com>
2023-04-05 16:41:58 +0500

    First Commit

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+// alert('Bano Qabil')
+// document.write('<h1>Hello</h1>')
+// console.log(123)
+
+var name = 'maira'
+console.log(name)
+
+alert('hello ' + name + ' how are you ' +  age)
+document.write('<h1>Hello</h1' )
+console.log(123)
+
+console.log(typeof name)
+
+var age = 23
+console.log(age)
+console.log(typeof age)
